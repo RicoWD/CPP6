66b50a82c6daf22c656bf69351e252dfdafdb369
erpascua <erpascua@student.42.fr>
2026-02-24 19:44:32 +0100

    (ex00): Preparing class ScarlarConvert.hpp

---
+++ ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 18:44:18 by erpascua          #+#    #+#             */
+/*   Updated: 2026/02/24 19:03:44 by erpascua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iomanip>
+#include <string>
+
+class ScalarConverter
+{
+	public:
+		static void convert(std::string input);
+}
