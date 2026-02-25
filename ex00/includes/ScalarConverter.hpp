cc811fe827009307c9f9ae50500254337765fbe2
RicoWD <pascual.machio.eric@gmail.com>
2026-02-25 05:45:09 +0100

    (ex00): HPP finished - colors, library, function, OCF

--- ex00/includes/ScalarConverter.hpp
+++ ex00/includes/ScalarConverter.hpp
@@ -6,17 +6,50 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 18:44:18 by erpascua          #+#    #+#             */
-/*   Updated: 2026/02/24 19:03:44 by erpascua         ###   ########.fr       */
+/*   Updated: 2026/02/25 04:51:31 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iomanip>
-#include <string>
+# include <climits>
+# include <iomanip>
+# include <iostream>
+# include <stdexcept>
+# include <string>
+# include <cmath>
+
+# define RESET		"\033[0m"
+# define GREEN		"\033[32m"
+# define RED		"\033[31m"
+# define GREY		"\033[90m"
+# define BOLDGREEN	"\033[1m\033[32m"
+# define BOLDRED	"\033[1m\033[31m"
+# define BOLDYELLOW	"\033[1m\033[33m"
 
 class ScalarConverter
 {
+	private:
+		// OCF (non instantiable)
+		ScalarConverter() {};
+		ScalarConverter(const ScalarConverter &other) { (void)other; };
+		ScalarConverter &operator=(const ScalarConverter &other) { (void)other; return *this; };
+		~ScalarConverter() {};
+	
 	public:
 		static void convert(std::string input);
-}
+};
+
+// Idenification functions
+int isChar(std::string input);
+int isInt(std::string input);
+int isFloat(std::string input);
+int isDouble(std::string input);
+
+// Print functions
+void printImpossible();
+void printChar(char c);
+void printInt(int n);
+void printFloat(float f);
+void printDouble(double d);
+void printDouble(double d);
